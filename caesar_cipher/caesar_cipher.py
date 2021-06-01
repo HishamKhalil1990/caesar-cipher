556bbde35b805bf60496b0f8f1ef4bd085346fa4
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-06-01 16:58:53 +0300

    lab 18 is finished

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,54 @@
+import nltk
+nltk.download('words',quiet=True)
+nltk.download('names',quiet=True)
+from nltk.corpus import words,names
+import re
+words_list = words.words()
+name_list = names.words()
+
+def encrypt(plain_text,key):
+    new_string = ""
+    for char in plain_text:
+        if re.match("[A-Za-z]",char):
+            new_char = 97 + ((ord(char.lower()) - 97) + key) % 26
+            new_string += chr(new_char)
+        else:
+            new_string += char
+    return new_string
+
+def decrypt(plain_text,key):
+    return encrypt(plain_text,-key)
+
+def crack(plain_text):
+    string_list1 = []
+    percentages_list = []
+    for num in range(27):
+        string_list1.append(decrypt(plain_text,-num))
+    string_list = [string.split(" ") for string in string_list1]
+    for str_list in string_list:
+        percentage = count(str_list) / len(str_list) * 100
+        percentages_list.append(percentage)
+    maximum_value = max(percentages_list)
+    index = percentages_list.index(maximum_value)
+    return string_list1[index]
+
+def count(str_list):
+    counter = 0
+    for word in str_list:
+        if word.lower() in words_list or word in name_list:
+            counter += 1
+    return counter
+
+if __name__ == "__main__":
+    encrepted = encrypt('A dead duck doesn\'t fly backward',13)
+    encrepted2 = encrypt('Cats are good pets, for they are clean and are not noisy',37)
+    encrepted3 = encrypt('Writing a list of random sentences is harder than I initially thought it would be',22)
+    encrepted4 = encrypt('He quietly entered the museum as the super bowl started',116)
+    print(encrepted)
+    print(encrepted2)
+    print(encrepted3)
+    print(encrepted4)
+    print(decrypt(encrepted,13))
+    print(decrypt(encrepted2,37))
+    print(crack(encrepted))
+    print(crack(encrepted2))
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,39 @@
+import pytest
 from caesar_cipher import __version__
-
+from caesar_cipher.caesar_cipher import encrypt,decrypt,crack
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_encrypt_one():
+    expected = 'n qrnq qhpx qbrfa\'g syl onpxjneq'
+    actual = encrypt('A dead duck doesn\'t fly backward',13)
+    assert actual == expected
+
+def test_encrypt_two():
+    expected = 'nled lcp rzzo aped, qzc espj lcp nwply lyo lcp yze yztdj'
+    actual = encrypt('Cats are good pets, for they are clean and are not noisy',37)
+    assert actual == expected
+
+def test_decrypt_one():
+    expected = 'writing a list of random sentences is harder than i initially thought it would be'
+    actual = decrypt('snepejc w heop kb nwjzki oajpajyao eo dwnzan pdwj e ejepewhhu pdkqcdp ep skqhz xa',22)
+    assert actual == expected
+
+def test_decrypt_two():
+    expected = 'he quietly entered the museum as the super bowl started'
+    actual = decrypt('tq cguqfxk qzfqdqp ftq ygeqgy me ftq egbqd naix efmdfqp',116)
+    assert actual == expected
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("uijt xbt b hppe ebz xjui zpv","this was a good day with you"),
+        ("xzmv dv pfli dfevp urizf","give me your money dario"),
+        ("p fjxti wdjht xh cxrt jcixa ndj pgt dgstgts id hipn xc xi udg bdciwh","a quiet house is nice until you are ordered to stay in it for months"),
+        ("r uxen njcrwp cxjbcnm lqnnbn jwm cdwj bjwmfrlqnb","i love eating toasted cheese and tuna sandwiches"),
+    ],
+)
+def test_crack(input,expected):
+    actual = crack(input)
+    assert actual == expected
